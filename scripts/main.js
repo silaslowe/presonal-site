1d7b4673472b855309df0a2dc6887788fd90acfa
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-31 14:22:26 -0500

    split string into a array

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import { renderHeader } from "./header.js"
 import { ProjectList } from "./projects/ProjectList.js"
+import { sorterer, dQuoter } from "./quotes/quoteList.js"
 
 renderHeader()
 ProjectList()
+sorterer()
+dQuoter()
